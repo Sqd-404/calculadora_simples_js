3dc0a0919a2d612d6d9236f18d12fe4148552626
Sownatta <wilsonn.rocha@outlook.com>
2023-10-12 22:37:02 -0300

    Fixed precision error in float numbers. Added a function to count decimals.

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,35 @@
 //Módulo que permite interação via terminal de maneira síncrona
 const readlineSync = require('readline-sync');
 
+
+//FUNCTIONS
+
 //@author: Wilson Rocha
-//@description: Função que recebe dois valores e retorna a subtração deles
-const calculadora = () => {
+//@description: A função recebe dois valores e retorna a subtração deles
+const calculadora = (n1, n2) => {
 
-    //const operator = readlineSync.question('Digita a operação desejeda: ');
+    let result;
 
-    const n1 = Number(readlineSync.question('Digite o primeiro numero: '));
-    const n2 = Number(readlineSync.question('Digite o segundo numero: '));
+    //const operator = readlineSync.question('Digite a operação desejeda: ');
+    
+    n1 = Number(readlineSync.question('Digite o primeiro numero: '));
+    n2 = Number(readlineSync.question('Digite o segundo numero: '));
+
+    const maxDecimals = Math.max(countDecimals(n1), countDecimals(n2)); //conta o número máximo de casas decimais dos número fornecidos
     
 
-    let result = n1 - n2;
+    result = (n1 - n2).toFixed(maxDecimals); //formata o número de casas decimais de acordo com o número máximo de casas decimais dos números fornecidos
 
     console.log(`Resultado: ${n1} - ${n2} = ${result}`);
 }
 
+//@author: Wilson rocha
+//@description: Função para contar as casas decimais em um número
+const countDecimals = (number) => {
+    if (Math.floor(number)=== number) return 0;
+    return number.toString().split('.')[1].length || 0;
+}
+
+
+//MAIN
 calculadora();
