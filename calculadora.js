6b4c7263550f033fbbc77a929bf1bfd912b52313
Sownatta <wilsonn.rocha@outlook.com>
2023-10-16 17:25:35 -0300

    Reorganizing file structure to align with project model.

--- calculadora.js
+++ calculadora.js
@@ -1,35 +1,60 @@
-//Módulo que permite interação via terminal de maneira síncrona
-const readlineSync = require('readline-sync');
-
-
-//FUNCTIONS
-
-//@author: Wilson Rocha
-//@description: A função recebe dois valores e retorna a subtração deles
-const calculadora = (n1, n2) => {
-
-    let result;
-
-    //const operator = readlineSync.question('Digite a operação desejeda: ');
-    
-    n1 = Number(readlineSync.question('Digite o primeiro numero: '));
-    n2 = Number(readlineSync.question('Digite o segundo numero: '));
-
-    const maxDecimals = Math.max(countDecimals(n1), countDecimals(n2)); //conta o número máximo de casas decimais dos número fornecidos
-    
-
-    result = (n1 - n2).toFixed(maxDecimals); //formata o número de casas decimais de acordo com o número máximo de casas decimais dos números fornecidos
-
-    console.log(`Resultado: ${n1} - ${n2} = ${result}`);
+//importando modulo readline-sync
+const readlineSync = require("readline-sync");
+//Criação do menu principal da calculadora
+/**
+ * @author Ilnara Ackermann
+ * @date 2023/10/13
+ * @description Menu básico para o funcionamento da calculadora
+ */
+function exibirMenu() {
+	while (true) {
+		console.log("Escolha uma opção:");
+		console.log("1 - Soma");
+		console.log("2 - Subtração");
+		console.log("3 - Multiplicação");
+		console.log("4 - Divisão");
+		console.log("5 - Exponenciação");
+		console.log("6 - Radiciação");
+		console.log("7 - Sair");
+		const opcao = readlineSync.question("Opção: ");
+		switch (opcao) {
+			case "1":
+				somar();
+				break;
+			case "2":
+				subtrair();
+				break;
+			case "3":
+				multiplicar();
+				break;
+			case "4":
+				dividir();
+				break;
+			case "5":
+				exponenciar();
+				break;
+			case "6":
+				radiciar();
+				break;
+			case "7":
+                console.log("Saindo do programa...");
+				return;
+			default:
+				console.log("Opção inválida, tente novamente.");
+		}
+	}
+}
+//função de exponenciação
+/**
+ * @author Ilnara Ackermann
+ * @date 2023/10/13
+ * @description função responsável pelo calculo de exponenciações
+ */
+function exponenciar() {
+	const num1 = readlineSync.question("numero base: ");
+	const num2 = readlineSync.question("numero do expoente: ");
+	const resultado = num1 ** num2;
+	console.log(resultado);
 }
 
-//@author: Wilson rocha
-//@description: Função para contar as casas decimais em um número
-const countDecimals = (number) => {
-    if (Math.floor(number)=== number) return 0;
-    return number.toString().split('.')[1].length || 0;
-}
-
-
-//MAIN
-calculadora();
+exibirMenu();
