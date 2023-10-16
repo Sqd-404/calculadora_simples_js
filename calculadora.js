b35f906b683f5ee3d75355c84621e36082177bd6
Luisa Nievas <luisa.nievas94@gmail.com>
2023-10-16 16:56:42 -0300

    Criada a função que executa a divisão da calculadora.

--- calculadora.js
+++ calculadora.js
@@ -50,8 +50,21 @@ function exibirMenu() {
  * @date 2023/10/13
  * @description função responsável pelo cálculo de divisão
  */
-function dividir() {
+
+const dividir = (num1, num2) =>{
+	let result;
+
+	num1 =  Number(readlineSync.question('Digite o primeiro número: '));
+	num2 =  Number(readlineSync.question('Digite o segundo número: '));
+
+	if( num1 && num2 !== 0 ){
+		result = num1 / num2
+		console.log(`Resultado: ${result}`);
+	}else{
+		console.log('Erro ao tentar executar a divisão. Verifique se inseriu os números corretamente.');
+	}
 
 }
 
-exibirMenu();
+// exibirMenu();
+dividir();
