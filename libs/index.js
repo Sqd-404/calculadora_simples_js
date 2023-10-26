a2f38540e2f5c1f0f368ebfdaf3c4f60cd792eef
Luisa Nievas <luisa.nievas94@gmail.com>
2023-10-26 13:56:31 -0300

    Exportando a função dividir no arquivo index

--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,8 @@
 const exponenciar = require("./exponenciar");
 const multiplyFunction = require('./multiplication.js');
 const multiplicar = multiplyFunction.multiplicar;
-const subtrair = require('./subtraction')
+const subtrair = require('./subtraction');
+const dividir = require('./division');
 
 
-module.exports = {exponenciar, multiplicar, subtrair}
+module.exports = {exponenciar, multiplicar, subtrair, dividir}
