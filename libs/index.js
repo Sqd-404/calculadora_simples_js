de8046b62c6f1eb41f783b0c6356271c0b6ce4a6
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-10-26 23:31:33 -0300

    update: menu fix: funcao multiplicar

--- libs/index.js
+++ libs/index.js
@@ -4,5 +4,6 @@ const multiplicar = multiplyFunction.multiplicar;
 const subtrair = require('./subtraction');
 const dividir = require('./division');
 const somar = require('./somar.js')
+const descobrirRaiz = require('./raiz')
 
-module.exports = {somar, exponenciar, multiplicar, subtrair, dividir}
+module.exports = {somar, exponenciar, multiplicar, subtrair, dividir, descobrirRaiz}
