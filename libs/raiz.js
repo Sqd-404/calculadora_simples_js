6d48596a3ec12c9f3e8c82fe113a046020f96642
Sownatta <wilsonn.rocha@outlook.com>
2023-10-27 15:22:07 -0300

    Ajuste na função descobrirRaiz

--- libs/raiz.js
+++ libs/raiz.js
@@ -9,14 +9,8 @@
 function descobrirRaiz(indice, radicando) {
 
     indice = Number(indice);
-    while (isNaN(indice) || indice < 1) {
-        console.log('O indice deve ser um numero e maior que 1, escolha um indice valido: ');
-    }
-    
     radicando = Number(radicando);
-    while (isNaN(radicando) || radicando < 0) {
-        console.log('O radicando deve ser um numero e nao pode ser negativo, escolha um radicando valido: ');
-    }
+  
     const resultado = radicando ** (1 / indice);
     console.log(`\nO resultado é: ${resultado}\n`);
 }
